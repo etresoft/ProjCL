dfa9e31502f316f759470e0daed12149d3898cff
etresoft <jdaniel@etresoft.com>
2020-09-28 15:28:12 -0400

    Add Framework project for testing. Go ahead and use Metal in name.

---
+++ ProjMetal/ProjMetal.h
@@ -0,0 +1,16 @@
+/***********************************************************************
+ ** Etresoft, Inc.
+ ** Copyright (c) 2020. All rights reserved.
+ **********************************************************************/
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProjMetal.
+FOUNDATION_EXPORT double ProjMetalVersionNumber;
+
+//! Project version string for ProjMetal.
+FOUNDATION_EXPORT const unsigned char ProjMetalVersionString[];
+
+// In this header, you should import all the public headers of your
+// framework using statements like #import <ProjMetal/PublicHeader.h>
+
